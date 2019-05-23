6c5b4fa5378a01e1c9a61881b902dd9c87c0f177
Renel Jean-Baptiste <reneljean123@gmail.com>
2019-05-22 22:15:48 -0400

    Create BooksWithException.java

---
+++ BooksWithException.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class BooksWithException_CTIM16801_Fall2016
+{
+public static void main(String[] args)
+{
+Scanner keyboard = new Scanner(System.in);
+int n = 0;
+String title = "";
+String author = "";
+int pages = 0;
+String category = "";
+boolean read = false;
+double cost = 0.0;
+boolean validN = false, validPages = false, validCategory = false,
+validCost = false, validRead = false;
+
+do
+{
+try
+{
+
+}//end try
+
+
+}
+
+}//end main
+
+
+
+
+}//end class
