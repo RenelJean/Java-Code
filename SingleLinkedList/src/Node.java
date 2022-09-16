983d59431fed701a7e783feb6cf7175e630c8967
Renel Jean-Baptiste <reneljean123@gmail.com>
2022-09-16 14:52:26 -0400

    Add files via upload

---
+++ SingleLinkedList/src/Node.java
@@ -0,0 +1,19 @@
+public class Node<E> {
+    private E element;
+    public Node<E> next;
+
+    public Node(E e, Node<E> next)
+    {
+        this.element = e;
+        this.next = next;
+    }
+
+    public Node<E> getNext() {return next;}
+
+    public E getElement() { return element;}
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+}
+
---
+++ SingleLinkedList/src/Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+    public static void main(String[] args) {
+        SingleLinkedList<String> list = new SingleLinkedList<String>();
+        String hello = "hello";
+        list.addFirst(hello);
+        list.addFirst("goodbye");
+        list.addFirst("firstelement");
+
+     //   list.printList();
+
+    }
+}
